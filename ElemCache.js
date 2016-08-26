f18348839c6d2ff7a0b746e65db7886f60703c1b
Jonathan Pacheco <jonathan@lifeblue.com>
2016-08-26 15:16:11 -0500

    (Mostly) Vanilla JS version. Need to normalize returned cached elms, though.

--- ElemCache.js
+++ ElemCache.js
@@ -1,32 +1,31 @@
-
 window.ElemCache = function () {
   var Self = this
   var settings = {}
   var objsCache = {}
-  var $container
+  var containerElem
 
   // performs the jQuery selection and then caches and returns it
   function cacheObj (key) {
     var elem = settings.elems[key]
-    var $parent
+    var parentElem
 
     // if the selector is an ID, don't bother with parent
     if (elem.selector.indexOf('#') === 0) {
-      objsCache[elem.key] = window.jQuery(elem.selector)
+      objsCache[key] = document.getElementById(elem.selector.substr(1))
 
-      return objsCache[elem.key]
+      return objsCache[key]
     }
 
     // set the parent to search in
     if (typeof (elem.parentKey) === 'string' && typeof (objsCache[elem.parentKey]) !== 'undefined') {
-      $parent = objsCache[elem.parentKey]
+      parentElem = objsCache[elem.parentKey]
     } else {
-      $parent = $container
+      parentElem = containerElem
     }
 
-    objsCache[elem.key] = $parent.find(elem.selector)
+    objsCache[key] = findElems(elem.selector, parentElem)
 
-    return objsCache[elem.key]
+    return objsCache[key]
   }
 
   // checks for a cached version of the jQuery object defined by `key` and returns it if found
@@ -56,11 +55,30 @@ window.ElemCache = function () {
     })
   }
 
+  function findElems (selector, parentElem) {
+    if (!parentElem) {
+      return false
+    }
+    if (parentElem.length > 1) {
+      var frag = document.createDocumentFragment()
+
+      ;[].forEach.call(parentElem, function (parent) {
+        var children = parent.querySelectorAll(selector)
+
+        ;[].forEach.call(children, function (child) {
+          frag.appendChild(child)
+        })
+      })
+      return frag.children
+    }
+    return parentElem.querySelectorAll(selector)
+  }
+
   // public
   Self.init = function (options) {
     var defaults = {
       elems: {},
-      container: 'body'
+      container: document.body
     }
     var key
 
@@ -68,7 +86,7 @@ window.ElemCache = function () {
     window.jQuery.extend(settings, defaults, options)
 
     // a global container as a default parent for searching
-    $container = window.jQuery(settings.container)
+    containerElem = typeof settings.container === 'string' ? document.querySelectorAll(settings.container) : settings.container
 
     // loops over the elems list and creates a getter for each
     for (key in settings.elems) {
